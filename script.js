45ceeaa9d17cfc421e82467a833cf7152f2e2bae
Yanghewon <hewon8883@naver.com>
2025-09-01 17:08:21 +0900

    update annotation

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const projects = document.querySelectorAll('.project');
-  projects.forEach(project => {
-    project.addEventListener('click', function () {
-      alert('프로젝트를 클릭했습니다!');
-    });
-  });
-});
